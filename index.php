aa7af7b9ef1547de03f299582831ee20cd360507
Takezo123 <134281625+Takezo123@users.noreply.github.com>
2024-02-05 13:24:07 +0300

    пихипи

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once "controllers\head.php";
+require_once "templates\header.php";
+require_once "templates\main.php";
+require_once "templates\do_futera.php";
+require_once "templates\hooter.php";
+?>
